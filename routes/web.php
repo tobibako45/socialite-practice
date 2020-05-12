e93cfc03342bfcaf8d588550421730931e22125e
tobibako45 <tobibako45@gmail.com>
2020-05-12 11:22:27 +0900

    initial

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+// Route::get('login/twitter', 'Auth\LoginController@redirectToTwitterProvider');
+// Route::get('login/twitter/callback', 'Auth\LoginController@handleTwitterProviderCallback');
